aaee72cc95ca16a62bc718ba0aaf154369fb72a3
darko <pegaz.int@siol.net>
2022-03-13 15:17:13 +0100

    Updates

---
+++ precache-manifest.bf78b60b10315e608e81301d75ceba10.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1dd37831ff7b2116af0",
+    "url": "/dcp/static/css/main.ecb50974.chunk.css"
+  },
+  {
+    "revision": "a1dd37831ff7b2116af0",
+    "url": "/dcp/static/js/main.a1dd3783.chunk.js"
+  },
+  {
+    "revision": "1f284f9bc76289c4db2f",
+    "url": "/dcp/static/js/1.1f284f9b.chunk.js"
+  },
+  {
+    "revision": "763b7b9e9692d0696575",
+    "url": "/dcp/static/js/2.763b7b9e.chunk.js"
+  },
+  {
+    "revision": "0b6173add312e1243f5d",
+    "url": "/dcp/static/js/runtime~main.0b6173ad.js"
+  },
+  {
+    "revision": "0601a26fb337ea6254694c14ee4da87f",
+    "url": "/dcp/static/media/beep-sound.0601a26f.mp3"
+  },
+  {
+    "revision": "052c4cfcde1f2f6fecaafcbcc100fddc",
+    "url": "/dcp/static/media/vakcine-1.052c4cfc.svg"
+  },
+  {
+    "revision": "e5f38cf50301ec1d88223c0db6487893",
+    "url": "/dcp/static/media/vakcine-2.e5f38cf5.svg"
+  },
+  {
+    "revision": "e9fdf3479ad5db0c19687160107933fd",
+    "url": "/dcp/static/media/recover-1.e9fdf347.svg"
+  },
+  {
+    "revision": "02c29a944f77db72cce9eead0b5e32cb",
+    "url": "/dcp/static/media/sledilnik.02c29a94.svg"
+  },
+  {
+    "revision": "49c930740e05ba8cfb841189d7d1e950",
+    "url": "/dcp/static/media/test-2.49c93074.svg"
+  },
+  {
+    "revision": "917481724c57cb584b946593a1c5ee08",
+    "url": "/dcp/static/media/recover-2.91748172.svg"
+  },
+  {
+    "revision": "e8b586f4c2107bf82f14a57ac49af3a1",
+    "url": "/dcp/static/media/test-1.e8b586f4.svg"
+  },
+  {
+    "revision": "334868a83a93a8bbd6cdfe0996d8dbc0",
+    "url": "/dcp/static/media/tripike.334868a8.svg"
+  },
+  {
+    "revision": "dae7cd60fc47ffdf9e8495fb59e7a5cc",
+    "url": "/dcp/static/media/qr_scan-2.dae7cd60.svg"
+  },
+  {
+    "revision": "6d755a11837f7f358ad3eb668f523a6d",
+    "url": "/dcp/static/media/arrowback.6d755a11.svg"
+  },
+  {
+    "revision": "dd0fba183cf4674b2d29f2c0df02717b",
+    "url": "/dcp/static/media/manual-2.dd0fba18.svg"
+  },
+  {
+    "revision": "32200cf6211c5b353e75552fede3ef4f",
+    "url": "/dcp/index.html"
+  }
+];
